12af5f559a489479f00ab4b3d4aca1c71242e38f
SonuRawat15 <147150872+SonuRawat15@users.noreply.github.com>
2023-10-26 06:02:06 +0530

    Deposit.java

---
+++ Deposit.java
@@ -0,0 +1,29 @@
+public class Deposit {
+    private int accountNumber;
+    private double amount;
+    private Date date; 
+    // Constructors
+    public Deposit(int accountNumber, double amount, Date date) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+        this.date = date;
+    }
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
